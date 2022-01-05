1d74602b0a7d7688cb69438584bb78afc311cb7a
SEO-J17 <qw4643987@naver.com>
2022-01-06 02:03:37 +0900

    refactoring code

--- src/완전탐색/덩치.kt
+++ src/완전탐색/덩치.kt
@@ -4,17 +4,17 @@ import java.util.*
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val num = br.readLine().toInt()
-    val person = Array(num, { Array(2, { 1 }) })
+    val person = Array(num) { Array(2) { 1 } }
     val sb = StringBuilder()
-    for (i in 0..num - 1) {
+    for (i in 0 until num) {
         val st = StringTokenizer(br.readLine())
         person[i][0] = st.nextToken().toInt()
         person[i][1] = st.nextToken().toInt()
     }
 
-    for (i in 0..num - 1) {
+    for (i in 0 until num) {
         var grade = 1
-        for (j in 0..num - 1) {
+        for (j in 0 until num) {
             if (person[i][0] < person[j][0] && person[i][1] < person[j][1]) {
                 grade++
             }
