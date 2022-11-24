0bffba382832617909783041c990c49bb136c512
SEO-J17 <qw4643987@naver.com>
2022-11-24 21:08:54 +0900

    solved 11-24

---
+++ src/프로그래머스_level_2/귤고르기.kt
@@ -0,0 +1,17 @@
+package 프로그래머스_level_2
+
+class Solution {
+    fun solution(k: Int, tangerine: IntArray): Int {
+        var answer: Int = 0
+        var limit = 0
+        tangerine.groupBy { it }.toList().sortedByDescending { it.second.size }.forEach {
+            if (limit >= k) {
+                return answer
+            }
+            limit += it.second.size
+            answer++
+        }
+
+        return answer
+    }
+}
